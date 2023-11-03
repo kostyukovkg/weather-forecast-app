cc7f0f362a7251d09599cda596823b12879b845b
Kostya <kostyukovkg@yahoo.com>
2023-11-03 14:32:53 +0300

    API key hided with "Secrets management" tool

--- backend.py
+++ backend.py
@@ -4,9 +4,6 @@ import streamlit as st
 # Everything is accessible via the st.secrets dict:
 a = st.write("API key:", st.secrets["api_key"])
 
-# 293 Video
-# key = "a5ac512a54654c8e7c9c1e1e6b57d781"
-
 def get_data(place, forecast_days=None):
     # Zhukovskiy 462755
     url = "http://api.openweathermap.org/data/2.5/forecast?" \
